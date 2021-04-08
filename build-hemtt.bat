a4aaece50e87a80a99bcdee8c337b03a119a121e
Salbei <marc.heinze@muenster.de>
2021-04-08 14:43:27 +0200

    fixed copy paste

--- build-hemtt.bat
+++ build-hemtt.bat
@@ -7,18 +7,18 @@ if exist a3 (
 mklink /j a3 include\a3
 
 mkdir x
-mkdir x\test_pzf3
-if exist x\test_pzf3\addons (
-  rmdir x\test_pzf3\addons
+mkdir x\GRAD_MOD_EXTRAS
+if exist x\GRAD_MOD_EXTRAS\addons (
+  rmdir x\GRAD_MOD_EXTRAS\addons
 )
-mklink /j x\test_pzf3\addons addons
+mklink /j x\GRAD_MOD_EXTRAS\addons addons
 
 hemtt build --force --release
 set BUILD_STATUS=%errorlevel%
 
 rmdir a3
-rmdir x\test_pzf3\addons
-rmdir x\test_pzf3
+rmdir x\GRAD_MOD_EXTRAS\addons
+rmdir x\GRAD_MOD_EXTRAS
 rmdir x
 
 if %BUILD_STATUS% neq 0 (
