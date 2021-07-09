ee4594af69ce70a2dcb79d69be9868e532db867c
DerZade <DerZade@gmail.com>
2021-07-09 14:00:41 +0200

    Update README.md, mod.cpp & hemtt.toml

--- mod.cpp
+++ mod.cpp
@@ -1,10 +1,10 @@
-name = "Gruppe Adler Mod Extras";
+name = "Gruppe Adler Extras";
 author = "Gruppe Adler";
 picture = "Adler_w_2.paa";
 actionName = "Website";
 action = "https://www.gruppe-adler.de/";
-description = "Gruppe Adler Mod Extras";
+description = "Gruppe Adler Extras";
 logo = "Adler_64.paa";
 logoOver = "Adler_w_64.paa";
-tooltip = "Gruppe Adler Mod Extras";
-overview = "Gruppe Adler Mod Extras that have RHS or TFAR dependencies.";
+tooltip = "Gruppe Adler Extras";
+overview = "Gruppe Adler Extras that have RHS or TFAR dependencies.";
