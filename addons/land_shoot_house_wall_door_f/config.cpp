6e96506c8b10f5af2f006e2ed0474f7f4c1521be
Salbei <salbei@gruppe-adler.de>
2021-07-11 17:31:48 +0200

    added Landshoothaus door

---
+++ addons/land_shoot_house_wall_door_f/config.cpp
@@ -0,0 +1,15 @@
+#define _ARMA_
+#include "script_component.hpp"
+
+class CfgPatches
+{
+	class Land_Shoot_House_Wall_Door_F
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_structures_f"};
+		author = "DerZade [A]";
+	};
+};
+#include "CfgVehicles.hpp"
---
+++ addons/land_shoot_house_wall_door_f/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT land_shoot_house_wall_door_f
+
+#include "\x\grad_extras\addons\main\script_mod.hpp"
+#include "\x\grad_extras\addons\main\script_macros.hpp"
